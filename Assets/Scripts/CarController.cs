e7026273739ee6efacd799c8f71232c815b7d18b
David Krénar <dawe484@gmail.com>
2021-02-25 20:56:08 +0100

    Working demo
    - Player car (move left or right)
    - Moving track (road)
    - Enemy cars

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float carSpeed;
+    private const float maxPos = 1.5f;
+
+    //Vector3 position;
+    private Vector3 targetPos;
+
+    private int desiredLane = 1; //0 - left; 1 - middle; 2 - right
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //position = transform.position;
+        targetPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+        //position.x = Mathf.Clamp(position.x, -maxPos, maxPos);
+        //transform.position = position;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if (desiredLane == 3) desiredLane = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1) desiredLane = 0;
+        }
+
+        targetPos.x = 0;
+
+        if (desiredLane == 0)
+        {
+            targetPos += Vector3.left * maxPos;
+        } else if (desiredLane == 2)
+        {
+            targetPos += Vector3.right * maxPos;
+        }
+
+        transform.position = targetPos;
+    }
+
+    // Car collision
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy Car"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyCarSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCarSpawner : MonoBehaviour
+{
+    public GameObject[] enemyCars;
+    private int enemyCarNumber;
+    public float maxSpawnPos = 1.5f;
+    public float delayTimer = 1f;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = delayTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Vector3 enemyCarPos = new Vector3(Random.Range(-maxSpawnPos, maxSpawnPos), transform.position.y, transform.position.z);
+            enemyCarNumber = Random.Range(0, enemyCars.Length-1);
+            // Debug.Log(enemyCarNumber);
+            Instantiate(enemyCars[enemyCarNumber], enemyCarPos, transform.rotation);
+            timer = delayTimer;
+        }
+
+    }
+}
